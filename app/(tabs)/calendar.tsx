8d8252d5af928f94494e716b53f9d7ceec8e2570
stephanotice <snsteinca@gmail.com>
2024-11-01 14:13:33 -0700

    getting rid of parallax scroll header for now

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Image, Button } from 'react-native';
-import { getCalendarDateString } from "react-native-calendars/src/services";
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -10,15 +9,7 @@ import React, { useEffect, useState } from 'react';
 import { MainCalendar } from '@/components/mainCalendar';
 import { useTabContext } from './TabContext';
 
-const addTodayToArray = (prevSelectedArray: string[]):string[] => {
-  const calendarFormatNow = getCalendarDateString(new Date())
-  if (!prevSelectedArray.includes(calendarFormatNow)) {
-    console.log('adding to array it should be selected now')
-      return [...prevSelectedArray,calendarFormatNow]
-} 
-console.log(`should already be in stored`) //not working for if i toggle off manually
-return prevSelectedArray
-}
+
 
 export default function calendarTabScreen() {
 
@@ -27,24 +18,23 @@ export default function calendarTabScreen() {
   
     return (
       
-      <ParallaxScrollView
+   /*    <ParallaxScrollView
         headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
         headerImage={
           <Image
             source={require('@/assets/images/partial-react-logo.png')}
           />
-        }>
+        }></ParallaxScrollView> */
+        <div>
         <ThemedView style={styles.titleContainer}>
           <ThemedText type="title">{"Hello World"}</ThemedText>
           <HelloWave />
         </ThemedView>
-        <MainCalendar selected={selected}/>
+        <MainCalendar/>
      <Button title="clear" onPress={clearAsyncStorage}>
   </Button>
-      </ParallaxScrollView>
-    );
-  }
-
+  </div>
+    )}
 const styles = StyleSheet.create({
   headerImage: {
     color: '#808080',
