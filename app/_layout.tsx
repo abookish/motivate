988da1f1c8762511ad1457682005df63d735b2af
stephanotice <snsteinca@gmail.com>
2024-11-01 21:51:49 -0700

    provider, no new style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,9 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { PaperProvider } from 'react-native-paper';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { TabProvider } from './(tabs)/TabContext';
 
@@ -28,13 +28,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <TabProvider>
+      <PaperProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      </PaperProvider>
       </TabProvider>
-    </ThemeProvider>
   );
 }
